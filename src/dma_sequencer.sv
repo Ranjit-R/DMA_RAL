0a0f0b55583ba17c82aa147e2a74b0554c4e1a45
Ranjith R <104053271+Ranjit-R@users.noreply.github.com>
2026-01-29 22:26:57 +0530

    Create dma_sequencer.sv

---
+++ src/dma_sequencer.sv
@@ -0,0 +1,18 @@
+class dma_sequencer extends uvm_sequencer #(dma_seq_item);
+  `uvm_component_utils(dma_sequencer)
+  
+  function new(string name = "dma_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+endclass
+
+class bus_sequencer extends uvm_sequencer #(dma_bus_item);
+  `uvm_component_utils(dma_sequencer)
+  
+  function new(string name = "dma_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+endclass
+
